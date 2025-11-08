874c86c54a7f68e114a8a0febe4557084db92129
Matheus Marchesotti <mmarferraz@gmail.com>
2025-11-07 23:19:25 -0300

    Add zig generators initial version

---
+++ src/gen/generator/zig/zigGenerator.cpp
@@ -0,0 +1,289 @@
+#include "zigGenerator.h"
+#include <string>
+#include <vector>
+
+void ZigGenerator::generateIncludes() {
+    includes.push_back("const std = @import(\"std\");");
+    
+    std::vector<std::string> varIncludes = VariableFactory::genIncludes(varType);
+    for (auto var : varIncludes) {
+        includes.push_back(var);
+    }
+}
+
+void ZigGenerator::generateGlobalVars() {
+    std::vector<std::string> varGlobalVars = VariableFactory::genGlobalVars(varType);
+    for (auto gVar : varGlobalVars) {
+        globalVars.push_back(gVar);
+    }
+    
+    globalVars.push_back("");
+    globalVars.push_back("// PRNG Global para execuções determinísticas");
+    globalVars.push_back("var prng = std.rand.DefaultPrng.init(0);");
+    globalVars.push_back("const random = prng.random();");
+}
+
+void ZigGenerator::generateRandomNumberGenerator() {
+    GeneratorFunction rngFunction = GeneratorFunction(-1);
+    rngFunction.addLine({
+        "pub fn get_path() !u64 {",
+        "    if (try std.process.getEnv(allocator, \"BENCH_PATH\")) |path| {",
+        "        defer allocator.free(path);",
+        "        return std.fmt.parseUnsigned(u64, path, 10);",
+        "    } else |err| {",
+        "        if (err == error.EnvironmentVariableNotFound) {",
+        "            return random.uint64();",
+        "        } else {",
+        "            return err;",
+        "        }",
+        "    }",
+        "}"
+    });
+    functions.push_back(rngFunction);
+}
+
+void ZigGenerator::generateMainFunction() {
+    mainFunction = GeneratorFunction(-1);
+    mainFunction.addLine({
+        "pub fn main() !void {",
+        "    // Garante que o alocador global seja limpo ao sair",
+        "    defer _ = gpa.deinit();", 
+        "",
+        "    var loopsFactor: i32 = 100;",
+        "    var path_seed: u64 = 0;",
+        "",
+        "    // Aloca e processa argumentos da linha de comando",
+        "    var args = try std.process.argsAlloc(allocator);",
+        "    defer std.process.argsFree(allocator, args);",
+        "",
+        "    var i: usize = 1;",
+        "    while (i < args.len) : (i += 1) {",
+        "        if (std.mem.eql(u8, args[i], \"-path-seed\")) {",
+        "            i += 1;",
+        "            if (i < args.len) { path_seed = std.fmt.parseUnsigned(u64, args[i], 10) catch 0; }",
+        "        } else if (std.mem.eql(u8, args[i], \"-loops-factor\")) {",
+        "            i += 1;",
+        "            if (i < args.len) { loopsFactor = std.fmt.parseInt(i32, args[i], 10) catch 100; }",
+        "        }",
+        "    }",
+        "    // Reinicializa o PRNG global com a semente fornecida",
+        "    prng = std.rand.DefaultPrng.init(path_seed);",
+        "}"
+    });
+    mainFunction.insertBack = true;
+    currentFunction.push(&mainFunction);
+    startScope();
+}
+
+void ZigGenerator::addLine(std::string line, int d) {
+    std::string indentedLine = currentScope.top().getIndentationTabs(d) + line;
+    currentFunction.top()->addLine(indentedLine);
+}
+
+void ZigGenerator::addLine(std::vector<std::string> lines, int d) {
+    for (auto line : lines) {
+        addLine(line, d);
+    }
+}
+
+void ZigGenerator::startScope() {
+    GeneratorScope scope = GeneratorScope(currentScope.top().avaiableVarsID, currentScope.top().avaiableParamsID, currentScope.top().getIndentation());
+    currentScope.push(scope);
+}
+
+void ZigGenerator::startFunc(int funcId, int nParameters) {
+    GeneratorFunction func = GeneratorFunction(funcId);
+    std::string funcHeader = "pub fn func" + std::to_string(funcId) + "(vars: *" + VariableFactory::genTypeString(varType) + "Param, ";
+    
+    for (int i = 0; i < nParameters; i++) {
+        funcHeader += "path" + std::to_string(i) + ": u64, ";
+    }
+    funcHeader += "loopsFactor: i32";
+    funcHeader += ") !" + VariableFactory::genTypeString(varType) + " {";
+    
+    func.addLine(funcHeader);
+    functions.push_back(func);
+    currentFunction.push(&(functions.back()));
+    GeneratorScope scope = GeneratorScope();
+    currentScope.push(scope);
+    this->ifCounter.push(0);
+    addLine("var pCounter: usize = vars.size;");
+}
+
+bool ZigGenerator::functionExists(int funcId) {
+    for (auto func : functions) {
+        if (func.getId() == funcId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::string ZigGenerator::createParams() {
+    std::string name = "params" + std::to_string(currentScope.top().addParam());
+    std::vector<GeneratorVariable*> varsParams;
+    for (int i = 0; i < (int)currentScope.top().avaiableVarsID.size(); i++) {
+        varsParams.push_back(variables[currentScope.top().avaiableVarsID[i]]);
+    }
+    std::vector<std::string> params = VariableFactory::genParams(varType, name, varsParams);
+    addLine(params);
+    return name;
+}
+
+void ZigGenerator::callFunc(int funcId, int nParameters) {
+    std::string param = createParams();
+    int id = addVar(varType);
+    GeneratorVariable* var = variables[id];
+    
+    std::string line = "var " + var->name + " = try func" + std::to_string(funcId) + "(&" + param + ", ";
+
+    for (int i = 0; i < nParameters; i++)
+        line += "try get_path(), ";
+    line += "loopsFactor";
+    line += ");";
+    addLine(line);
+
+    addLine("defer allocator.free(" + param + ".data);");
+}
+
+int ZigGenerator::addVar(std::string type) {
+    this->variables[varCounter] = VariableFactory::createVariable(type, varCounter);
+    this->currentScope.top().addVar(varCounter);
+    return varCounter++;
+}
+
+void ZigGenerator::freeVars(bool hasReturn, int returnVarPos) {
+    int numberOfAddedVars = currentScope.top().numberOfAddedVars;
+    std::vector<int> availableVarsId = currentScope.top().avaiableVarsID;
+    for (int i = 0; i < numberOfAddedVars; i++) {
+        int varPos = availableVarsId.size() - i - 1;
+        if (!hasReturn || varPos != returnVarPos) {
+            GeneratorVariable* var = variables[availableVarsId[varPos]];
+            addLine(var->free());
+        }
+    }
+}
+
+void ZigGenerator::returnFunc(int returnVarPos) {
+    GeneratorVariable* var = variables[currentScope.top().avaiableVarsID[returnVarPos]];
+    addLine("return " + var->name + ";");
+}
+
+void ZigGenerator::endScope() {
+    std::string line = currentScope.top().getIndentationTabs(-1) + "}";
+    currentFunction.top()->addLine(line);
+    currentScope.pop();
+}
+
+void ZigGenerator::endFunc() {
+    endScope();
+    currentFunction.pop();
+    ifCounter.pop();
+}
+
+void ZigGenerator::genMakefile(std::string dir, std::string target) {
+    std::ofstream buildFile;
+    buildFile.open(dir + "build.zig");
+    buildFile << "const std = @import(\"std\");\n\n";
+    buildFile << "pub fn build(b: *std.Build) {\n";
+    buildFile << "    const target = b.standardTargetOptions(.{});\n";
+    buildFile << "    const optimize = b.standardOptimizeOption(.{});\n\n";
+    buildFile << "    const exe = b.addExecutable(.{\n";
+    buildFile << "        .name = \"" + target + "\",\n";
+    buildFile << "        .root_source_file = b.path(\"src/main.zig\"),\n";
+    buildFile << "        .target = target,\n";
+    buildFile << "        .optimize = optimize,\n";
+    buildFile << "    });\n\n";
+    buildFile << "    b.installArtifact(exe);\n\n";
+    buildFile << "    const run_step = b.step(\"run\", \"Run the app\");\n";
+    buildFile << "    const run_exe = b.addRunArtifact(exe);\n";
+    buildFile << "    run_exe.step.dependOn(&exe.step);\n";
+    buildFile << "    run_step.dependOn(&run_exe.step);\n";
+    buildFile << "}\n";
+    buildFile.close();
+}
+
+void ZigGenerator::genReadme(std::string dir, std::string target) {
+    std::ofstream readme;
+    readme.open(dir + "README.md");
+    readme << "# " + target + " Program (Zig)\n\n";
+    readme << "This program was generated by the **BenchGen** tool.\n\n";
+    readme << "## Compilation\n\n";
+    readme << "To compile the program, use the Zig build system:\n\n";
+    readme << "    ```bash\n";
+    readme << "    zig build\n";
+    readme << "    ```\n\n";
+    readme << "    O executável estará em `zig-out/bin/" + target + "`.\n\n";
+    readme << "## Run\n\n";
+    readme << "Para compilar e executar o programa de uma só vez:\n\n";
+    readme << "```bash\n";
+    readme << "zig build run\n";
+    readme << "```\n\n";
+    readme << "Para executar o programa compilado com argumentos:\n\n";
+    readme << "```bash\n";
+    readme << "./zig-out/bin/" + target + " -loops-factor 50 -path-seed 123\n";
+    readme << "```\n\n";
+    readme << "### Optional Arguments\n\n";
+    readme << "-   `-path-seed <seed>`: Sets the seed for the random number generator. Default is `0`.\n\n";
+    readme << "-   `-loops-factor <factor>`: Sets the factor for the number of loops. Default is `100`.\n\n";
+}
+
+void ZigGenerator::generateFiles(std::string benchmarkName) {
+    std::string benchDir = benchmarkName + "/";
+    std::string sourceDir = benchDir + "src/";
+
+    std::filesystem::create_directory(benchDir);
+    std::filesystem::create_directory(sourceDir);
+
+    this->genMakefile(benchDir, benchmarkName);
+    this->genReadme(benchDir, benchmarkName);
+
+    std::ofstream libFile;
+    libFile.open(sourceDir + "lib.zig");
+    
+    for (auto include : includes) {
+        libFile << include << std::endl;
+    }
+    libFile << std::endl;
+
+    for (auto var : globalVars) {
+        libFile << var << std::endl;
+    }
+    libFile << std::endl;
+
+    for (auto func : functions) {
+        auto lines = func.getLines();
+        for (auto line : lines) {
+            libFile << line << std::endl;
+        }
+        libFile << std::endl;
+    }
+    libFile.close();
+
+    std::ofstream mainFile;
+    mainFile.open(sourceDir + "main.zig");
+    
+    mainFile << "const lib = @import(\"lib.zig\");\n\n";
+    
+    mainFile << "const std = lib.std;\n";
+    mainFile << "const allocator = lib.allocator;\n";
+    mainFile << "var gpa = lib.gpa;\n";
+    mainFile << "var prng = lib.prng;\n";
+    mainFile << "const random = lib.random;\n";
+    mainFile << "const " + VariableFactory::genTypeString(varType) + " = lib." + VariableFactory::genTypeString(varType) + ";\n";
+    mainFile << "const " + VariableFactory::genTypeString(varType) + "Param = lib." + VariableFactory::genTypeString(varType) + "Param;\n";
+    mainFile << "const get_path = lib.get_path;\n";
+
+    for (auto func : functions) {
+        if (func.getId() != -1) {
+            mainFile << "const func" + std.to_string(func.getId()) + " = lib.func" + std::to_string(func.getId()) + ";\n";
+        }
+    }
+    mainFile << std.endl;
+
+    auto mainLines = mainFunction.getLines();
+    for (auto line : mainLines) {
+        mainFile << line << std::endl;
+    }
+    mainFile.close();
+}
---
+++ src/gen/generator/zig/zigGeneratorVariable.cpp
@@ -0,0 +1,120 @@
+#include "zigGeneratorVariable.h"
+
+ZigGeneratorArray::ZigGeneratorArray(int size, int id) {
+    this->typeString = "Array";
+    this->totalSize = size;
+    this->id = id;
+    this->name = VarTypes::ARRAY + std::to_string(id);
+}
+
+ZigGeneratorArray::~ZigGeneratorArray() {}
+
+std::vector<std::string> ZigGeneratorArray::new_(bool inFunction) {
+    std::vector<std::string> temp = {"var " + this->name + ": *" + this->typeString + " = undefined;"};
+
+    if (inFunction) {
+        temp.push_back("if (pCounter > 0) {");
+        temp.push_back("    pCounter -= 1;");
+        temp.push_back("    " + this->name + " = vars.data[pCounter];");
+        temp.push_back("    " + this->name + ".refC += 1;");
+        temp.push_back("    // DEBUG_COPY(" + this->name + ".id);");
+        temp.push_back("} else {");
+        temp.push_back("    " + this->name + " = try allocator.create(" + this->typeString + ");");
+        temp.push_back("    " + this->name + ".*.size = " + std::to_string(this->totalSize) + ";");
+        temp.push_back("    " + this->name + ".*.refC = 1;");
+        temp.push_back("    " + this->name + ".*.id = " + std::to_string(this->id) + ";");
+        temp.push_back("    " + this->name + ".*.data = try allocator.allocZ(u32, " + this->name + ".*.size);");
+        temp.push_back("    // DEBUG_COPY(" + this->name + ".id);");
+        temp.push_back("}");
+    } else {
+        temp.push_back(this->name + " = try allocator.create(" + this->typeString + ");");
+        temp.push_back(this->name + ".*.size = " + std::to_string(this->totalSize) + ";");
+        temp.push_back(this->name + ".*.refC = 1;");
+        temp.push_back(this->name + ".*.id = " + std::to_string(this->id) + ";");
+        temp.push_back(this->name + ".*.data = try allocator.allocZ(u32, " + this->name + ".*.size);");
+        temp.push_back("    // DEBUG_NEW(" + this->name + ".id);");
+    }
+    return temp;
+}
+
+std::vector<std::string> ZigGeneratorArray::insert() {
+    std::vector<std::string> temp = {"for (0.." + this->name + ".size) |i| {"};
+    temp.push_back("    " + this->name + ".data[i] += 1;");
+    temp.push_back("}");
+    return temp;
+}
+
+std::vector<std::string> ZigGeneratorArray::remove() {
+    std::vector<std::string> temp = {"for (0.." + this->name + ".size) |i| {"};
+    temp.push_back("    " + this->name + ".data[i] -= 1;");
+    temp.push_back("}");
+    return temp;
+}
+
+std::vector<std::string> ZigGeneratorArray::contains(bool shouldReturn) {
+    int compare = rand() % 100;
+    std::vector<std::string> temp = {};
+    temp.push_back("for (0.." + this->name + ".size) |i| {");
+    temp.push_back("    if (" + this->name + ".data[i] == " + std::to_string(compare) + ") {");
+    if (shouldReturn) {
+        temp.push_back("        return " + this->name + ";");
+    } else {
+        temp.push_back("        " + this->name + ".data[i] += " + std::to_string(compare) + ";");
+    }
+    temp.push_back("    }");
+    temp.push_back("}");
+    return temp;
+}
+
+std::vector<std::string> ZigGeneratorArray::free() {
+    std::vector<std::string> temp = {};
+    temp.push_back(this->name + ".refC -= 1;");
+    temp.push_back("if (" + this->name + ".refC == 0) {");
+    temp.push_back("    allocator.free(" + this->name + ".data);");
+    temp.push_back("    allocator.destroy(" + this->name + ");");
+    temp.push_back("    // DEBUG_FREE(" + this->name + ".id);");
+    temp.push_back("}");
+    return temp;
+}
+
+std::vector<std::string> ZigGeneratorArray::genIncludes() {
+    return {};
+}
+
+std::vector<std::string> ZigGeneratorArray::genGlobalVars() {
+    std::vector<std::string> temp = {};
+    temp.push_back("const std = @import(\"std\");");
+    temp.push_back("");
+    temp.push_back("// DEBUG_FREE(" + this->name + ".id);");
+    temp.push_back("var gpa = std.heap.GeneralPurposeAllocator(.{}){};");
+    temp.push_back("const allocator = gpa.allocator();");
+    temp.push_back("");
+    temp.push_back("const " + this->typeString + " = struct {");
+    temp.push_back("    data: []u32,");
+    temp.push_back("    size: usize,");
+    temp.push_back("    refC: usize,");
+    temp.push_back("    id: i32,");
+    temp.push_back("};");
+    temp.push_back("");
+    temp.push_back("const " + this->typeString + "Param = struct {");
+    temp.push_back("    data: []*" + this->typeString + ","); 
+    temp.push_back("    size: usize,");
+    temp.push_back("};");
+    return temp;
+}
+
+std::vector<std::string> ZigGeneratorArray::genParams(std::string paramName, std::vector<GeneratorVariable*> varsParams) {
+    std::vector<std::string> temp = {};
+    std::string dataSliceName = paramName + "_data";
+
+    temp.push_back("var " + dataSliceName + " = try allocator.alloc(*" + this->typeString + ", " + std::to_string(varsParams.size()) + ");");
+    for (int i = 0; i < (int)varsParams.size(); i++) {
+        temp.push_back(dataSliceName + "[" + std::to_string(i) + "] = " + varsParams[i]->name + ";");
+    }
+
+    temp.push_back("var " + paramName + " = " + this->typeString + "Param{");
+    temp.push_back("    .data = " + dataSliceName + ",");
+    temp.push_back("    .size = " + std::to_string(varsParams.size()) + ",");
+    temp.push_back("};");
+    return temp;
+}
